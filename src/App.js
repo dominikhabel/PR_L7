25b462bb18f658c8eec64e70ca434f50863a2785
dominikhab <dominik.habel7@gmail.com>
2021-04-30 12:13:45 +0200

    PR_L8

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import NavBar from "./components/common/navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+     <div className="container-fluid">
+       <NavBar />
+       <div className="container">
+           <div className="content">
+               <Switch>
+                   <Route path="/posts"
+                   component={Posts} />}
+                   />
+                   <Route path="/" exact component={Home} />
+               </Switch>
+           </div>
+       </div>
+     </div>
+ );
 }
 
 export default App;
